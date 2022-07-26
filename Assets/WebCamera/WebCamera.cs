cb7f6a60456ffd210ce40d1103d016238e39f16b
MomokaFujita <momoka.fujita@somethinggood.jp>
2022-07-26 13:41:36 +0900

    SnapCameraを使えるように

--- Assets/WebCamera/WebCamera.cs
+++ Assets/WebCamera/WebCamera.cs
@@ -8,7 +8,13 @@ public class WebCamera : MonoBehaviour
     public RawImage RawImage;
     WebCamTexture webCam;
 
-    public void OnClick()
+    private static int INPUT_SIZE = 256;
+    private static int FPS = 30;
+
+    // カメラの選択
+    int selectCamera = 0;
+
+    private void Start()
     {
         WebCamDevice[] devices = WebCamTexture.devices; //接続されているウェブカメラを取得
 
@@ -23,5 +29,22 @@ public class WebCamera : MonoBehaviour
         RawImage.texture = webCam;
         //カメラ表示開始
         webCam.Play();
+    }
+
+    public void OnClick()
+    {
+        // カメラの取得
+        WebCamDevice[] webCamDevice = WebCamTexture.devices;
+
+        // カメラが1個の時は無処理
+        if (webCamDevice.Length <= 1) return;
+
+        // カメラの切り替え
+        selectCamera++;
+        if (selectCamera >= webCamDevice.Length) selectCamera = 0;
+        this.webCam.Stop();
+        this.webCam = new WebCamTexture(webCamDevice[selectCamera].name,INPUT_SIZE, INPUT_SIZE, FPS);
+        this.RawImage.texture = this.webCam;
+        this.webCam.Play();
     }
 }
